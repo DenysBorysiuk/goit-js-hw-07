56691fa5341aa6f044c78822a5c48600c4a49010
DenysBorysiuk <denisborisiuk@gmail.com>
2022-10-27 17:46:09 +0300

    process_01-task

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,28 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryRefs = document.querySelector('.gallery');
+const galleryMarkup = createMarkup(galleryItems);
 
-console.log(galleryItems);
+galleryRefs.insertAdjacentHTML('beforeend', galleryMarkup);
+galleryRefs.addEventListener('click', onClick);
+
+function createMarkup(galleryMarkup) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>`;
+    })
+    .join('');
+}
+
+function onClick(evt) {
+  evt.preventDefault();
+  console.dir(evt.target.dataset.source);
+}
+// console.log(galleryItems);
