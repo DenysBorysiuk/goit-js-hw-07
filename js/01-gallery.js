12f0a139d3de5ac3f6d9f42d81f47b8c326df1d6
DenysBorysiuk <denisborisiuk@gmail.com>
2022-10-27 23:30:41 +0300

    bugfix_task01

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -12,14 +12,14 @@ function createMarkup(gallery) {
   return gallery
     .map(
       ({ preview, original, description }) =>
-        `<a class="gallery__link" href="${original}">
+        `<div class="gallery__item"><a class="gallery__link" href="${original}">
     <img
       class="gallery__image"
       src="${preview}"
       data-source="${original}"
       alt="${description}"
     />
-  </a>`
+  </a></div>`
     )
     .join('');
 }
